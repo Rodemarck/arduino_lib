b711e57e36eb223e02cd359d88283d7024cdf8e0
rodemarck <rodemarck.meloj@ufrpe.br>
2022-04-19 16:40:21 -0300

    atualização

--- Expansor.h
+++ Expansor.h
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#define bitClear(a,b) a&=~(1<<b)
+#define bitSet(a,b) a|=(1<<b)
+#define bitVal(a,b) !!(a & (1<< b))
+#define setBit(a,b,c) {bool var_10001=bitVal(a,b);if(var_10001!=c) (c) ? bitSet(a,b); : bitClear(a,b);}
+#define DELAY_PULSE 5
 
 class Saida{
 protected:
@@ -16,6 +21,7 @@ public:
     void begin(uint8_t ci_n, uint8_t latch_p, uint8_t data_p, uint8_t clock_p);
     void atualiza();
     void escreve(int p, bool val);
+    bool valor(int p);
 };
 Saida* Saida::Saida(uint8_t ci_n, uint8_t latch_p, uint8_t data_p, uint8_t clock_p){
     pinMode(latch_p,OUTPUT);
@@ -29,26 +35,74 @@ Saida* Saida::Saida(uint8_t ci_n, uint8_t latch_p, uint8_t data_p, uint8_t clock
     int i = 0;
     for(;i < ci_n; i++)
         this->dados[i] = 0;
+    this->atualiza();
 }
 void Saida::escreve(int p, bool val){
     int c = p / 8;
     int k = p % 8;
-    if(val)
-        bitSet(this->dados[c],p);
-    else
-        bitClear(this->dados[c],p);
+    Saida* t = Saida::GetInstance();
+    setBit(t->dados[c],k,val);
+    t->atualiza();
+}
+bool Saida::valor(int p){
+    int c = p / 8;
+    int k = p % 8;
+    return !!(Saida::GetInstance()->dados[c] & (1 << k));
 }
 void Saida::atualiza(){
-    digitalWrite(this->latch_p, LOW);
-    var i = ci_n - 1;
+    Saida* t = Saida::GetInstance();
+    digitalWrite(t->latch_p, LOW);
+    uint8_t i = ci_n - 1;
     for(; i >=0; i--)
-        shiftOut(this->data_p, this->clock_p, LSBFIRST, this->dados[i]);
-    digitalWrite(this->latch_p, HIGH);
+        shiftOut(t->data_p, t->clock_p, LSBFIRST, t->dados[i]);
+    digitalWrite(t->latch_p, HIGH);
 }
 Saida* Saida::GetInstance(){
-    return singleton_;
+    return Saida::singleton_;
+}
+void Saida::begin(uint8_t ci_n, uint8_t latch_p, uint8_t data_p, uint8_t clock_p){
+    if(Saida::singleton_ != nullptr){
+        free(Saida::singleton_->dados);
+        free(Saida::singleton_);
+    }
+    Saida::singleton_ = new Saida::Saida(ci_n, latch_p, data_p, clock_p));
 }
 
 class Entrada{
+protected:
+    Entrada(uint8_t ci_n, uint8_t latch_p, uint8_t data_p, uint8_t clock_p, uint8_t enable_p);
+    static Entrada* singleton_ = nullptr;
+    uint8_t ci_n;
+    uint8_t latch_p;
+    uint8_t data_p;
+    uint8_t clock_p;
+    uint8_t enable_p;
+    uint8_t* dados;
+    static Entrada *GetInstance();
+public:
+    Entrada(Entrada &other) = delete;
+    void operator=(const Entrada &) = delete;
+    void begin(uint8_t ci_n, uint8_t latch_p, uint8_t data_p, uint8_t clock_p, uint8_t enable_p);
+    void atualiza();
+    void escreve(int p, bool val);
+    bool valor(int p);
+}
 
+void Entrada::atualiza(){
+    Entrada* e = Entrada::GetInstance();
+    digitalWrite(e->enable_p,HIGH);
+    digitalWrite(e->latch_p,LOW);
+    delayMicroseconds(DELAY_PULSE);
+    digitalWrite(e->latch_p,HIGH);
+    digitalWrite(e->enable_p,LOW);
+    uint8_t val;
+    for(uint8_t c = e->ci_n-1; c>=0; c--){
+        for(uint8_t k = 7; k>=0; k--){
+            val = digitalRead(e->data_p);
+            setBit(e->dados[c],k,val);
+            digitalWrite(e->clock_p,LOW);
+            delayMicroseconds(DELAY_PULSE);
+            digitalWrite(e->clock_p,HIGH);
+        }
+    }
 }
